01b0bacbe400bbae2342c46350ba26b97979a6c0
dsanch1120 <sanchez@mymail.mines.edu>
2020-07-27 17:58:26 -0600

    Added function that allows user to indicate number of bits for calculation

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 //Map that will clear the terminal screen
 var clear map[string]func()
 var addition bool
+var bits int
 
 func pow(base int, exp int) int{
 	output := 1
@@ -50,6 +51,36 @@ func CallClear() {
 	if ok {
 		value()
 	}
+}
+
+//Gets number of bits
+func getBits() {
+	//Clears Terminal
+	CallClear()
+
+	//Prompts user to input desired number of bits
+	fmt.Println("Enter Number of Bits")
+
+	//Loop that iterates until user gives correct input
+	for {
+		reader := bufio.NewReader(os.Stdin)
+
+		//Gets user's input
+		temp, _ := reader.ReadString('\n')
+		temp = strings.Replace(temp, "\n", "", -1)
+		int, err := strconv.Atoi(temp)
+
+		//Ensures user gives a correct input
+		if err == nil && (0 < int) {
+			bits = int
+			break
+		} else {
+			CallClear()
+			fmt.Println("Invalid input")
+			fmt.Println("Enter Number of Bits as an Integer")
+		}
+	}
+	CallClear()
 }
 
 //Gets binary strings
@@ -102,6 +133,22 @@ func decimalToBinary(num int, option int) string{
 		if len(bin) == 0 {
 			return "000"
 		}
+		break
+	case 2:
+		if num < 0 {
+			bin = append(bin, 1)
+			num *= -1
+		} else {
+			bin = append(bin, 0)
+		}
+		for {
+			if num <= 0 {
+				break
+			}
+			bin = append(bin, num%2)
+			num = num / 2
+		}
+		break
 	}
 	output := ""
 	for i := len(bin) - 1; i >= 0; i-- {
@@ -127,6 +174,9 @@ func unsigned() {
 
 	fmt.Println()
 
+	getBits()
+
+	//Gets values of binary strings to be used
 	bin1 := getStrings(1)
 	bin2 := getStrings(2)
 
@@ -135,6 +185,7 @@ func unsigned() {
 	val1 := 0
 	val2 := 0
 
+	//Converts from binary to base 10
 	for i := len(bin1) - 1; i >= 0; i-- {
 		if bin1[i] == '1' {
 			val1 += pow(2, bitCounter)
@@ -149,19 +200,23 @@ func unsigned() {
 		bitCounter++
 	}
 
+	//Performs the appropriate arithmetic
 	if addition {
 		dec = val1 + val2
 	} else {
 		dec = val1 - val2
 	}
 
+	//Converts from base 10 to binary
 	output := decimalToBinary(dec, 1)
 
+	//Displays output to user
 	fmt.Println(output)
 }
 
 //Calculates for signed binary
 func signed() {
+	//Message to be displayed
 	CallClear()
 	if addition {
 		fmt.Println("Signed Binary Addition")
@@ -171,11 +226,63 @@ func signed() {
 
 	fmt.Println()
 
+	getBits()
+
+	//Gets binary values to be used throughout function
 	bin1 := getStrings(1)
 	bin2 := getStrings(2)
 
-	fmt.Println(bin1)
-	fmt.Println(bin2)
+	//Converts from signed binary to base 10
+	bitCounter := 0
+	dec := 0
+	sign := 0
+	val1 := 0
+	val2 := 0
+
+	//Converts from binary to base 10
+	for i := len(bin1) - 1; i >= 0; i-- {
+		if i == 0 {
+			if bin1[0] == '0' {
+				sign = 1
+			} else {
+				sign = -1
+			}
+			break
+		}
+		if bin1[i] == '1' {
+			val1 += pow(2, bitCounter)
+		}
+		bitCounter++
+	}
+	val1 *= sign
+	bitCounter = 0
+	for i := len(bin2) - 1; i >= 0; i-- {
+		if i == 0 {
+			if bin2[0] == '1' {
+				sign = 1
+			} else {
+				sign = -1
+			}
+			break
+		}
+		if bin2[i] == '1' {
+			val2 += pow(2, bitCounter)
+		}
+		bitCounter++
+	}
+	val2 *= sign
+	//Performs the appropriate arithmetic
+	if addition {
+		dec = val1 + val2
+	} else {
+		dec = val1 - val2
+	}
+
+	//Converts from base 10 to signed binary
+	output := decimalToBinary(dec, 2)
+
+	//Displays output to user
+	fmt.Println(output)
 }
 
 //Allows the user to choose their conditions
